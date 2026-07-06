5c2ebde025b0d214fe4e8cf6968c88622579b277
athiyashafa <athiyashafa959@gmail.com>
2026-07-06 18:47:56 +0700

    aplikasi penjualan

---
+++ src/koneksi/UserID.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+/**
+ *
+ * @author Athiya
+ */
+public class UserID {
+    private static String userLogin;
+
+    public static String getUserLogin() {
+        return userLogin;
+    }
+
+    public static void setUserLogin(String user) {
+        userLogin = user;
+    }
+}
